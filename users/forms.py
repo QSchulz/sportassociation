41b397f9fd52044da50ec9498cd3378b16912c70
Quentin Schulz <quentin.schulz@utbm.fr>
2015-08-30 14:42:23 +0200

    Clean user form.

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
-from .models import (GENDERS, SCOPES, SCOPE_REGISTERED, SCOPE_MANAGER,
-                    SCOPE_STAFF, SCOPE_MEMBER, SHIRT_SIZES)
-from django.core.validators import RegexValidator
-from django.forms.extras.widgets import SelectDateWidget
-from datetime import datetime
-
-GENDERS_PLUS_EMPTY = (('', '-------'),) + GENDERS
-SHIRT_SIZES_PLUS_EMPTY = (('', '-------'),) + SHIRT_SIZES
 
 class AdminUserForm(forms.Form):
 
