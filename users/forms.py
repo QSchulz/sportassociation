e1d21844e0e883e51befc151ce7fac4ebab3e55d
Quentin Schulz <quentin.schulz@utbm.fr>
2015-10-30 16:46:44 +0100

    Update French translation.

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 class AdminUserForm(forms.Form):
 
-    email = forms.EmailField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    id_photo = forms.ImageField()
+    email = forms.EmailField(label=_('Email'))
+    first_name = forms.CharField(label=_('First name'))
+    last_name = forms.CharField(label=_('Last name'))
+    id_photo = forms.ImageField(label=_('Identity photo'))
